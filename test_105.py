69577b78ab508e2768814dd2d5a3c280abe8cda4
chenxing <chenxing>
2023-07-25 16:14:59 -0700

    test

--- test_105.py
+++ test_105.py
@@ -657,10 +657,10 @@ def make_ano_save_output():
             #filename = DATASET_PATH + 'final-outputs/ARGS=' + str(args["arg_num"]) + '/output_result/'+str(i)+'.png'
             #filename_img = DATASET_PATH + 'final-outputs/ARGS=' + str(args["arg_num"]) + '/output_img/' + str(i) + '.png'
             #filename_pt = DATASET_PATH + 'final-outputs/ARGS=' + str(args["arg_num"]) + '/output_pt/' + str(i) + '.pt'
-            filename_pt = DATASET_PATH + 'output/latent_train_noise/' + str(i) + '.pt'
+            filename_pt = DATASET_PATH + 'output/latent/' + str(i) + '.pt'
             output = diff.forward_backward(
                     unet, img,
-                    see_whole_sequence="from_noise",
+                    see_whole_sequence="half",
                     # t_distance=5, denoise_fn=args["noise_fn"]
                     t_distance=t_distance, denoise_fn=args["noise_fn"]
                     )
